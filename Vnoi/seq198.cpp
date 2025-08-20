f1250d23a99468d64256535b3257a5989e0a262c
catashiro31 <catashiro31@gmail.com>
2025-08-21 00:02:47 +0700

    Day: 21/8/2025

--- Vnoi/seq198.cpp
+++ Vnoi/seq198.cpp
@@ -21,46 +21,51 @@ const string NoF = "Name_of_File";
     
 void solve() {
     int m; cin >> m;
-    vector<int> arr(m);
-    unordered_map<int,int> c;
-    for (int &x : arr) {
-        cin >> x;
-        c[x]++;
-    }
-    sort(all(arr));
-    set<pair<int,int>> store;
+    map<int,int> freq;
     for (int i = 0; i < m; i++) {
-        auto it = store.lb({arr[i],INT_MIN});
-        if (it == store.end()) store.insert({arr[i],1});
-        else {
-            auto [val,cnt] = *it;
-            store.erase(it), store.insert({val,cnt+1});
+        int x; cin >> x;
+        freq[x]++;
+    }
+    int n = sz(freq);
+    vector<int> values;
+    for (auto [v,f] : freq) values.psb(v); 
+    int MAX_MASK = 1 << 10;
+    ll dp[n][MAX_MASK];
+    for (int mask = 0; mask < MAX_MASK; mask++) dp[0][mask] = LLONG_MAX;
+    dp[0][0] = 0;
+    for (int i = 0; i < n; i++) {
+        for (int mask = 0; mask < MAX_MASK; mask++) {
+            if (dp[i][mask] < 0) continue;
+            int shift = 0;
+            if (i == 0) shift = 0;
+            else {
+                int gap = values[i] - values[i-1];
+                if (gap > 9) shift = 0;
+                else {
+                    shift = mask << gap;
+                    shift = shift & (MAX_MASK-1);
+                }
+            }
+            int new_mask = (shift << 1) & (MAX_MASK - 1);
+            dp[i+1][new_mask] = min(dp[i+1][new_mask], dp[i][mask]);
+
+            bool can_keep = true;
+            int d[3] = {1, 8, 9};
+            for (int x : d) {
+                if (x <= 9 && (shift & (1 << x) != 0)) {
+                    can_keep = false;
+                    break;
+                }
+            }
+            if (can_keep) {
+                new_mask = ((shift << 1) | 1) & (MAX_MASK - 1);
+                dp[i+1][new_mask] = max(dp[i+1][new_mask], dp[i][mask] + freq[values[i]]);
+            }
         }
     }
-    unordered_map<int,int> umap;
-    int d[3] = {1, 8, 9};
-    for (auto [val,cnt] : store) {
-        for (int i = 0; i < 3; i++) {
-            auto it = store.lb({val+d[i],INT_MIN});
-            if (it == store.end()) continue;
-            if ((*it).fi == val+d[i]) umap[val] += (*it).se, umap[val+d[i]] += (*it).se;
-        }
-    }
-    vector<int> tmp;
-    for (auto [val, cnt] : umap) tmp.psb(val);
-    sort(all(tmp), [&](auto &a, auto &b) {
-        return umap[a] > umap[b];
-    });
     int kq = 0;
-    for (int x : tmp) {
-        if (umap[x] <= 0) continue;
-        for (int i = 0; i < 3; i++) {
-            auto it1 = store.lb({x+d[i],INT_MIN});
-            auto it2 = store.lb({x-d[i],INT_MIN});
-            if (it1 !=store.end() && (*it1).fi == x+d[i]) umap[x+d[i]] -= c[x];
-            if ((*it2).fi == x-d[i]) umap[x-d[i]] -= c[x];
-        }
-        kq += c[x];
+    for (int i = 0; i < n; i++) {
+        for (int mask = 0; mask < MAX_MASK; mask++) kq = max(kq, dp[i][mask] + freq[values[i]]);
     }
     cout << kq;
 }
